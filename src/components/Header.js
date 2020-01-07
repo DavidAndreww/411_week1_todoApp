e65606237673383d47df29559cd4251d3a79afaf
David Bille <d_bille@yahoo.com>
2020-01-06 23:06:20 -0600

    working on it

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+    <h1>My To Do List</h1>
+    <h3>Get shit done!</h3>
+    </header>
+  )
+}
+
+export default Header;
